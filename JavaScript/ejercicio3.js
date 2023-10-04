4e5c2d77f4278b72d37150569fe28594d52b933a
David Quintero Garcia <davidquinterocentro@hotmail.com>
2023-10-03 20:55:02 -0600

    new Exercise in JS!

---
+++ JavaScript/ejercicio3.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+ */
+
+
+
+function solution(inputString) {
+    var poli = "";
+    for(var i = inputString.length - 1; i>=0; i--){
+        poli += inputString[i];
+    }
+    return poli == inputString;
+}
